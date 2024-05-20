0b0feaf5a8b5fb3054ca3b53b74023444c295c90
Skrebov <e11801320@student.tuwien.ac.at>
2024-05-20 09:58:05 +0200

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
         // Return the JSX
         return (
           <main>
-            <h1>My todos</h1>
+            <h1>{user?.signInDetails?.loginId}'s todos</h1>
             <button onClick={createTodo}>+ new</button>
             <ul>
               {todos.map((todo) => (

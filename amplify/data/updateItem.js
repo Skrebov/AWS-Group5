3bb52f89cb4d517466ccc697e5e436508303b6b2
Damian Baranik <damian.baranik@bighub.cz>
2024-06-14 17:56:45 +0200

    Maybe this will work

--- amplify/data/updateItem.js
+++ amplify/data/updateItem.js
@@ -1,35 +1,24 @@
-// /**
-//  * Request handler for the AppSync resolver.
-//  *
-//  * @param {object} ctx - The context object provided by AppSync.
-//  * @returns {object} - The request object formatted for DynamoDB Query operation.
-//  */
-// export function request(ctx) {
-//   const { pk, sk, ...values } = ctx.args;
-//   const updateExpression = 'SET ' + Object.keys(values).map(key => `${key} = :${key}`).join(', ');
-//   const updateExpression = 'SET ' + Object.entries(values).map(([key, value]) => `${key} = ${value }`).join(', ');
-//   return {
-//       operation: 'UpdateItem',
-//       key: util.dynamodb.toMapValues({ pk, sk }),
-//       update: {
-//         expression: updateExpression,
-//         expressionValues: util.dynamodb.toMapValues(values),
-//       }
-//   };
-// }
-import * as ddb from "@aws-appsync/utils/dynamodb";
-
+/**
+ * Request handler for the AppSync resolver.
+ *
+ * @param {object} ctx - The context object provided by AppSync.
+ * @returns {object} - The request object formatted for DynamoDB Query operation.
+ */
 export function request(ctx) {
-  const { pk, sk, ...rest } = ctx.args;
-  const values = Object.entries(rest).reduce((obj, [key, value]) => {
-    obj[key] = value ?? ddb.operations.remove();
-    return obj;
+  const { pk, sk, ...values } = ctx.args;
+  const updateExpression = 'SET ' + Object.keys(values).map(key => `${key} = :${key}`).join(', ');
+  const updateExpressionValues = Object.entries(values).reduce((acc, [key, value]) => {
+    acc[`:${key}`] = value;
+    return acc;
   }, {});
-
-  return ddb.update({
-    key: { pk, sk },
-    update: { ...values },
-  });
+  return {
+      operation: 'UpdateItem',
+      key: util.dynamodb.toMapValues({ pk, sk }),
+      update: {
+        expression: updateExpression,
+        expressionValues: util.dynamodb.toMapValues(updateExpressionValues),
+      }
+  };
 }
 
 /**

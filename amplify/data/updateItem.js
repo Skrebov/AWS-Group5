4b006b39f1cd227d8c0ecfc65a19ba3fe85cc6a8
Damian Baranik <damian.baranik@bighub.cz>
2024-06-14 17:48:47 +0200

    Documentation stuff again

--- amplify/data/updateItem.js
+++ amplify/data/updateItem.js
@@ -1,21 +1,35 @@
-/**
- * Request handler for the AppSync resolver.
- *
- * @param {object} ctx - The context object provided by AppSync.
- * @returns {object} - The request object formatted for DynamoDB Query operation.
- */
+// /**
+//  * Request handler for the AppSync resolver.
+//  *
+//  * @param {object} ctx - The context object provided by AppSync.
+//  * @returns {object} - The request object formatted for DynamoDB Query operation.
+//  */
+// export function request(ctx) {
+//   const { pk, sk, ...values } = ctx.args;
+//   const updateExpression = 'SET ' + Object.keys(values).map(key => `${key} = :${key}`).join(', ');
+//   const updateExpression = 'SET ' + Object.entries(values).map(([key, value]) => `${key} = ${value }`).join(', ');
+//   return {
+//       operation: 'UpdateItem',
+//       key: util.dynamodb.toMapValues({ pk, sk }),
+//       update: {
+//         expression: updateExpression,
+//         expressionValues: util.dynamodb.toMapValues(values),
+//       }
+//   };
+// }
+import * as ddb from "@aws-appsync/utils/dynamodb";
+
 export function request(ctx) {
-  const { pk, sk, ...values } = ctx.args;
-  const updateExpression = 'SET ' + Object.keys(values).map(key => `${key} = :${key}`).join(', ');
-  
-  return {
-      operation: 'UpdateItem',
-      key: util.dynamodb.toMapValues({ pk, sk }),
-      update: {
-        expression: updateExpression,
-        expressionValues: util.dynamodb.toMapValues(values),
-      }
-  };
+  const { pk, sk, ...rest } = ctx.args;
+  const values = Object.entries(rest).reduce((obj, [key, value]) => {
+    obj[key] = value ?? ddb.operations.remove();
+    return obj;
+  }, {});
+
+  return ddb.update({
+    key: { pk, sk },
+    update: { ...values },
+  });
 }
 
 /**
@@ -25,5 +39,3 @@ export function request(ctx) {
  * @returns {object} - The result object directly from the DynamoDB query response.
  */
 export const response = (ctx) => ctx.result;
-
-

6191de6cda434aecfa90761c0c69c58c36dd4a15
Ahmed El Agrod <e11811340@student.tuwien.ac.at>
2024-06-24 00:12:24 +0200

    added scrollbar if products overflows

--- src/components/shared/product-modal.tsx
+++ src/components/shared/product-modal.tsx
@@ -10,11 +10,11 @@ type Props = {
 };
 
 const ProductModal: FunctionComponent<Props> = ({
-                                                        customerId,
-                                                        closeModal,
-                                                        getData,
-                                                        mode
-                                                   }) => {
+                                                    customerId,
+                                                    closeModal,
+                                                    getData,
+                                                    mode
+                                                }) => {
     const [productsData, setProductsData] = useState<Product[]>([]);
     useEffect(() => {
         const fetchProducts = async () => {
@@ -28,6 +28,7 @@ const ProductModal: FunctionComponent<Props> = ({
     const getProducts = async (id: string) => {
         return await getData(id)
     }
+
     function formatPrice(amount: number) {
         return new Intl.NumberFormat("en-US", {
             style: "currency",
@@ -41,32 +42,34 @@ const ProductModal: FunctionComponent<Props> = ({
                 title={mode === 'invoice' ? "Products of " + customerId : "Recommended Products"}
                 className="space-y py-4 text-center"
             />
-            <table className="min-w-full divide-y divide-gray-200">
-                <thead>
-                <tr>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Category</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Quantity</th>
-                </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200">
-                {productsData.length > 0 ? (
-                    productsData.map((product, index) => (
-                        <tr key={index}>
-                            <td className="px-6 py-4 whitespace-nowrap">{product.name}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{product.category}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{formatPrice(+product.price)}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{product.quantity}</td>
-                        </tr>
-                    ))
-                ) : (
-                    <tr className="px-6 py-4">
-                        <td>Loading...</td>
+            <div className="overflow-y-auto h-96">
+                <table className="min-w-full divide-y divide-gray-200">
+                    <thead>
+                    <tr>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Category</th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price</th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Quantity</th>
                     </tr>
-                )}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200">
+                    {productsData.length > 0 ? (
+                        productsData.map((product, index) => (
+                            <tr key={index}>
+                                <td className="px-6 py-4 whitespace-nowrap">{product.name}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{product.category}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{formatPrice(+product.price)}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{product.quantity}</td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr className="px-6 py-4">
+                            <td>Loading...</td>
+                        </tr>
+                    )}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 

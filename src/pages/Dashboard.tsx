68a21fdd0e1a972bc9819e49cbb8fc5a162cff99
Ahmed El Agrod <e11811340@student.tuwien.ac.at>
2024-06-23 23:59:47 +0200

    added button to open sidebar in mobile view

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -20,13 +20,13 @@ export default function Dashboard({
             <Sidebar/>
             <div className="flex w-0 flex-1 flex-col overflow-hidden">
                 <div className="relative z-10 flex h-20 flex-shrink-0  shadow">
-                    {/*<button*/}
-                    {/*    className="pl-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 xl:hidden"*/}
-                    {/*    onClick={() => setSidebarOpen(true)}*/}
-                    {/*>*/}
-                    {/*    <span className="sr-only">Open sidebar</span>*/}
-                    {/*    <MenuIcon className="h-6 w-6" aria-hidden="true"/>*/}
-                    {/*</button>*/}
+                    <button
+                        className="pl-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 lg:hidden"
+                        onClick={() => setSidebarOpen(true)}
+                    >
+                        <span className="sr-only">Open sidebar</span>
+                        <MenuIcon className="h-6 w-6" aria-hidden="true"/>
+                    </button>
                     <Header/>
                 </div>
                 {children}

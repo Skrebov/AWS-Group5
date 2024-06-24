5829e0f28898678d10c88b512d63361adc98f274
Skrebov <e11801320@student.tuwien.ac.at>
2024-06-24 19:51:40 +0200

    fix: add esnext browser target

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext' //browsers can handle the latest ES features
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
